5bb5ec19cca4e67a6bdefeda26813b3fdc97b5e7
Jan Dusek <j@koiru.com>
2019-02-19 12:33:28 +0800

    Added trainee's public SSH key

--- c9_init.sh
+++ c9_init.sh
@@ -1,2 +1,11 @@
 # Script to be run during C9 initialization
 date > /tmp/c9_init.log
+
+# add public key
+if [ ! -d ~/.ssh ]; then
+  mkdir ~/.ssh
+  chmod 700 ~/.ssh
+done
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqVrhdUVXBc36dtSDsi7N3STDN1oScx+BPH4alFrleLA+04fo4LAqULqQf1m4WB2tqPsaRQ0NJmiLb3zKs8Mx63tIeJcutuZDokSNs/AOOEINV3kG9y52vdqSqZb9rwulLiS1JqMAi74O/8/hG73uGWkzGZjeviSVM5scwoBT9QBjpzCamaUGJbI4dCWuM26sbXEFavbuen3gC154ssBIAa/0OX9SAYbZoZ/xP3I0rR7o1U925lOHZORxmf8cpnCDShSeyVqmD/ln8j2KaS5uK4Muu8S9cSUE0jJQQ2pxa9CEhWHeRNHTFuZMw6jyhx8Fx5t6anPK3xobpobafNPY1 trainer@twilio" >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
