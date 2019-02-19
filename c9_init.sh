c86dc4778e9c90c12dec90429f632f03d777a2ae
Jan Dusek <j@koiru.com>
2019-02-19 13:32:55 +0800

    Bash typo

--- c9_init.sh
+++ c9_init.sh
@@ -5,7 +5,7 @@ date > /tmp/c9_init.log
 if [ ! -d ~/.ssh ]; then
   mkdir ~/.ssh
   chmod 700 ~/.ssh
-done
+fi
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqVrhdUVXBc36dtSDsi7N3STDN1oScx+BPH4alFrleLA+04fo4LAqULqQf1m4WB2tqPsaRQ0NJmiLb3zKs8Mx63tIeJcutuZDokSNs/AOOEINV3kG9y52vdqSqZb9rwulLiS1JqMAi74O/8/hG73uGWkzGZjeviSVM5scwoBT9QBjpzCamaUGJbI4dCWuM26sbXEFavbuen3gC154ssBIAa/0OX9SAYbZoZ/xP3I0rR7o1U925lOHZORxmf8cpnCDShSeyVqmD/ln8j2KaS5uK4Muu8S9cSUE0jJQQ2pxa9CEhWHeRNHTFuZMw6jyhx8Fx5t6anPK3xobpobafNPY1 trainer@twilio" >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
